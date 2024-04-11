c944c04804bba356d537496a8abf0852267399d2
Luiz Serão <luiz.serao@hotmail.com>
2024-04-11 06:27:21 -0300

    home best services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BestServices from "@/components/Home/BestServices/BestServices";
 import HeroHome from "@/components/Home/Hero/HeroHome";
 
 export default function Home() {
   return (
     <main>
       <HeroHome />
+      <BestServices />
     </main>
   );
 }
